28b214ed64fcd1e47167f6cef0b17f68c06b4448
kashish88 <kashish1449.cse18@chitkara.edu.in>
2020-08-06 01:47:23 +0530

    initial commit

---
+++ scriptc.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.coffee-menu').on('click',function(){
+        $('.toggle').toggleClass('open');
+        $('.nav-list').toggleClass('open');
+    } );
+    AOS .init({
+        easing:'ease',
+        duration:1000
+    })
+})
